d960720799bc4859007aa3a75976efb389eb92c1
Chelsea Schmidt <cschmidt331@gmail.com>
2020-09-17 18:31:53 -0700

    Handle form submission and add README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Technical Challenge for Just Utah Coders
+
+## System requirements
+  - Node (version 10.20.1 used in development)
+  - npm package manager
+  - Nodemon (version 2.0.4 used in development)
+
+## Run the application
+  - npm install
+  - npm run build
+  - npm start
+    - Optional environment variable: PORT (defaults to 3000)
+  - Navigate to http://localhost:3000
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-did-update-set-state */
 import React from 'react';
+import axios from 'axios';
 import TextInput from './TextInput';
 import { checkIfEmailValid, checkIfDateValid } from '../utilities/helpers';
 import '../styles/App.css';
@@ -13,10 +14,12 @@ export default class App extends React.Component {
       birthdate: '',
       agree: false,
       submittable: false,
+      success: false,
     };
     this.handleTextInput = this.handleTextInput.bind(this);
     this.handleUserAgreement = this.handleUserAgreement.bind(this);
     this.clearForm = this.clearForm.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -43,13 +46,14 @@ export default class App extends React.Component {
     }
   }
 
-  clearForm() {
+  clearForm(success) {
     this.setState({
       name: '',
       email: '',
       birthdate: '',
       agree: false,
       submittable: false,
+      success,
     });
   }
 
@@ -63,6 +67,31 @@ export default class App extends React.Component {
     this.setState({ agree: checked });
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    const {
+      name,
+      email,
+      birthdate,
+      agree,
+    } = this.state;
+    axios({
+      method: 'post',
+      url: 'https://my-json-server.typicode.com/JustUtahCoders/interview-users-api/users',
+      data: {
+        name,
+        email,
+        birthDate: birthdate,
+        emailConsent: agree,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        this.clearForm(true);
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
     const {
       name,
@@ -70,6 +99,7 @@ export default class App extends React.Component {
       birthdate,
       agree,
       submittable,
+      success,
     } = this.state;
     const fields = ['Name', 'Email', 'Birth Date'];
     const values = [name, email, birthdate];
@@ -101,10 +131,18 @@ export default class App extends React.Component {
             I agree to be contacted via email. *
           </label>
           <div id="button-area">
-            <button type="button" onClick={this.clearForm}>Clear</button>
-            <button type="submit" disabled={!submittable}>Submit</button>
+            <button type="button" onClick={() => this.clearForm(false)}>Clear</button>
+            <button
+              form="contact-form"
+              type="submit"
+              disabled={!submittable}
+              onClick={this.handleSubmit}
+            >
+              Submit
+            </button>
           </div>
           <div id="footnote">* Indicates required field</div>
+          {success && <div id="success-message">Success - thank you!</div>}
         </form>
       </div>
     );
